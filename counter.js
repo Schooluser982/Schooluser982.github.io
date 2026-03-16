ed17e721986aba27ec98439293890b8af4162cbc
Schooluser982 <gamerroblox29G@outlook.com>
2026-03-16 18:56:05 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-function loadVisits() {
+document.addEventListener("DOMContentLoaded", () => {
   fetch("https://api.countapi.xyz/hit/schooluser982.github.io/site")
     .then(res => res.json())
     .then(data => {
@@ -10,6 +10,4 @@ function loadVisits() {
       if (el) el.innerText = "Error";
       console.error("Visit counter error:", err);
     });
-}
-
-document.addEventListener("DOMContentLoaded", loadVisits);
+});
