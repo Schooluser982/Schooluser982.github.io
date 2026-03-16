c04c5cd4c4cfe2ff41649f7699163e544a3a5e85
Schooluser982 <gamerroblox29G@outlook.com>
2026-03-16 11:12:40 -0400

    Update button.js

--- button.js
+++ button.js
@@ -1,1 +1,17 @@
+function openGame(gameFile) {
+  // Open a blank tab
+  const newTab = window.open("about:blank", "_blank");
 
+  // Fetch the game HTML file
+  fetch(`/games/${gameFile}`)
+    .then(response => response.text())
+    .then(html => {
+      newTab.document.open();
+      newTab.document.write(html);
+      newTab.document.close();
+    })
+    .catch(err => {
+      newTab.document.write("<h1>Error loading game</h1>");
+      console.error(err);
+    });
+}
