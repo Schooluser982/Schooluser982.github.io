79cf12b16048e1c63fad8887cf5965610456ace9
Schooluser982 <gamerroblox29G@outlook.com>
2026-03-17 07:41:21 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://hits.sh/schooluser982.github.io.json")
+  fetch("https://raw.githubusercontent.com/Schooluser982/Schooluser982.github.io/main/visits.json")
     .then(res => res.json())
     .then(data => {
       const el = document.getElementById("visitCount");
-      if (el) el.innerText = data.hits;
+      if (el) el.innerText = data.count;
     })
     .catch(err => {
       const el = document.getElementById("visitCount");
